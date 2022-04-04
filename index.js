76454fa8e7905b334eecbffec3469ec2c476033b
Genesis Agravio <agraviog@gmail.com>
2022-04-04 16:29:40 +0800

    wavesurfer barRadius 1

--- index.js
+++ index.js
@@ -447,7 +447,7 @@ Webflow.push(function () {
           progressColor: "#00fff6",
           cursorColor: "transparent",
           height: 24,
-          barRadius: 4,
+          barRadius: 1,
         });
         wavesurfer.load(convertedFiles.original);
         wavesurfer.setVolume(0);
