7d91c747dd626a0948ea3d155a9d64583d7f5363
Genesis Agravio <agraviog@gmail.com>
2022-04-08 22:54:09 +0800

    nplayer 2

--- newplayer.js
+++ newplayer.js
@@ -16,8 +16,23 @@ wavesurfer.load(
   "https://uceb3aa33f3fc9684005e35e7ad2.dl.dropboxusercontent.com/cd/0/inline/BjDNbm_cmVhriZdKw-UdEbap4-jmcQrRCTiYBXw67TuV-l1wfAdteRj7k3fwfu_FHKQxvzzn-jbhwnRzBZXVgrkz2oWd-dqXM1IYyGGLTickTrQQJIqry-SlsP1agkwhaHplELcl_Mg_JUr4GobYiaM3Gs6IjN7EbMs0SiczUND5Ww/file#"
 );
 
-plyBtn.onclick = function () {
-  wavesurfer.playPause();
+function toggleIcon() {
   $(".pause_icon-nplayer").toggleClass("played");
   $(".play_icon-nplayer").toggleClass("paused");
-};
+}
+
+$(".control_play-nplayer").on("click", function () {
+  if (!$(".control_play-share").hasClass("control_disable")) {
+    isPlaying = !isPlaying;
+    if (shouldResetWavesurfer && isPlaying) {
+      wavesurfer.seekTo(0);
+      shouldResetWavesurfer = false;
+    }
+    if (isPlaying) {
+      wavesurfer.play();
+    } else {
+      wavesurfer.pause();
+    }
+    toggleIcon();
+  }
+});
